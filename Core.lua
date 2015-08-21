4ce04ef9b93076f962f5b0a382a6febde8a301d5
Hendrik Leppkes <h.leppkes@gmail.com>
2015-08-21 14:46:02 +0200

    Use HandyNotes continent lists instead of accessing Astrolabe directly

--- Core.lua
+++ Core.lua
@@ -33,7 +33,6 @@ local UIParent = _G.UIParent
 local WorldMapButton = _G.WorldMapButton
 local WorldMapTooltip = _G.WorldMapTooltip
 
-local Astrolabe = DongleStub("Astrolabe-1.0")
 local HandyNotes = _G.HandyNotes
 local TomTom = _G.TomTom
 
@@ -151,21 +150,6 @@ do
 end
 
 do
-	local continentMapFile = {
-		["Kalimdor"]              = {__index = Astrolabe.ContinentList[1]},
-		["Azeroth"]               = {__index = Astrolabe.ContinentList[2]}, -- Eastern Kingdoms
-		["Expansion01"]           = {__index = Astrolabe.ContinentList[3]}, -- Outland
-		["Northrend"]             = {__index = Astrolabe.ContinentList[4]},
-		["TheMaelstromContinent"] = {__index = Astrolabe.ContinentList[5]},
-		["Vashjir"]               = {[0] = 613, 614, 615, 610},
-		["Pandaria"]              = {__index = Astrolabe.ContinentList[6]},
-		["Draenor"]               = {__index = Astrolabe.ContinentList[7]},
-	}
-
-	for k, v in pairs(continentMapFile) do
-		setmetatable(v, v)
-	end
-
 	-- custom iterator we use to iterate over every node in a given zone
 	local function iter(t, prestate)
 		if not completedQuests[38356] or not completedQuests[37961] then return end
@@ -207,7 +191,7 @@ do
 			end
 
 			-- get next zone
-			zone = zone + 1
+			zone = next(t.C, zone)
 			t.Z = zone
 			mapFile = HandyNotes:GetMapIDtoMapFile(t.C[zone])
 			prestate = nil
@@ -215,10 +199,10 @@ do
 	end
 
 	function AzerothsTopTunes:GetNodes(mapFile)
-		local C = continentMapFile[mapFile] -- is this a continent?
+		local C = HandyNotes:GetContinentZoneList(mapFile) -- Is this a continent?
 
 		if C then
-			local tbl = { C = C, Z = 0 }
+			local tbl = { C = C, Z = next(C) }
 			return iterCont, tbl, nil
 		else
 			mapFile = gsub(mapFile, "_terrain%d+$", "")
